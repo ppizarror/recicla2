1783976c9989fda4a2ef7c43bae6e3da52d77e4e
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-08-06 02:51:39 +0100

    Borra contenido antiguo php

--- docs/jsdoc/.jsdoc.js
+++
@@ -1,9 +0,0 @@
-/**
- * RECICLA2
- * Archivo .js básico para llamar a esdoc.
- *
- * @author Pablo Pizarro R. @ppizarror.com
- * @license Copyright 2018, no copiar o distribuír sin permiso directo del autor.
- */
-
-console.log('');
--- gendocs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cls
-ECHO CREANDO DOCUMENTACION - JSDOC3
-jsdoc docs\jsdoc\.jsdoc.js -c docs\jsdoc\config.json
-ECHO PROCESO TERMINADO
-@echo on
