5e54828b1e5449b17861d402f7bdef134afa55e4
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-08-06 03:26:16 +0100

    Genera estructura general tarea en servlets intellij

---
+++ src/com/recicla2/TestServlet.java
@@ -0,0 +1,16 @@
+package com.recicla2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.write("<h1>holi</h1");
+        out.flush();
+    }
+}
